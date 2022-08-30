5a6f8791d4e4e68dad4407427a5a984af7a7674d
M4gictrick <68494147+M4gictrick@users.noreply.github.com>
2022-08-30 22:21:10 +0700

    Update run.py
    test loop with time check

--- run.py
+++ run.py
@@ -5,22 +5,38 @@ if platform.system() == "Linux" or platform.system() == "Darwin":
     os.environ["KIVY_VIDEO"] = "ffpyplayer"
     
 from pysimbotlib.core import PySimbotApp, Robot
+from kivy.logger import Logger
 from kivy.config import Config
 # Force the program to show user's log only for "info" level or more. The info log will be disabled.
 Config.set('kivy', 'log_level', 'info')
 
 import random
 
+# update robot every 0.5 seconds (2 frames per sec)
+REFRESH_INTERVAL = 1/2
+
+
 class RandomWalkRobot(Robot):
     
     def update(self):
-        r = random.randint(0, 3)
-        self.move(5)
-        if r == 1:
-            self.turn(15)
-        elif r == 2:
-            self.turn(-15)
+        self.turn(self.smell())
+        while True:
+            IR = self.distance()
+            print(IR[0])
+            if IR[0] <= 20:
+                IR = self.distance()
+                self.turn(-2)
+            elif IR[1] <= 20:
+                IR = self.distance()
+                self.turn(-2)
+            elif IR[7] <= 20:
+                IR = self.distance()
+                self.turn(2)
+            else:
+                self.move(2)
+                break
+
 
 if __name__ == '__main__':
-    app = PySimbotApp(robot_cls=RandomWalkRobot, num_robots=2)
+    app = PySimbotApp(robot_cls=RandomWalkRobot, num_robots=1,interval=REFRESH_INTERVAL)
     app.run()
